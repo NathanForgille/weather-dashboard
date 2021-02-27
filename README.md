4714e027150cdbbab1b2b24b5fd59605f010add9
Jean Ralphio <nateforgille@outlook.com>
2021-02-26 21:38:38 -0600

    codes is all finished. local storage is storing but not populating.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Credit
 - ![Youtube video for date format manipulation](https://www.youtube.com/watch?v=SU1jqW-n9PI&ab_channel=CryptersInfotech)
+- ![Youtube video for date format manipulation](https://www.youtube.com/watch?v=8IEI-7fj2j4&ab_channel=JuniorDeveloperCentral)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //global variable the user provides from their search
 let searchValue;
+let citySearched = localStorage.getItem("city");
 // the main function to fetch the initial weather forecast data and append it to the page
 function weather() {
     //personal weather api with a searchValue key from the user
@@ -11,16 +12,16 @@ function weather() {
         }).then(function (data) {
             // this line empties the forecast and allows the new search to populate in the previous searches place
             $("#forecast").empty();
+            $("#today").empty();
             // this line grabs the latitude and longitude for the uvIndex function
             let latitude = data.city.coord.lat;
             let longitude = data.city.coord.lon;
             uvIndex(latitude, longitude);
             // variable to increse id for cardInit by 1
-            let j= 0;
-        //this for loop runs through the array, slices the time slots to provide days, and appends them using DOM manipulation
+            let j = 0;
+            //this for loop runs through the array, slices the time slots to provide days, and appends them using DOM manipulation
             for (let i = 0; i < 40; i += 8) {
                 let days = data.list[i];
-                console.log(data);
                 // id for cardInit to increse by 1 so that the uvIndex can follow the id
                 let cardInit = $(`<div id="day-${j}">`).addClass("whole");
                 j++
@@ -43,26 +44,36 @@ function uvIndex(latitude, longitude) {
         .then(function (uvResponse) {
             return uvResponse.json();
         }).then(function (uvData) {
-            for (let i = 0; i < 8; i++){
-            var uv = uvData.daily[i].uvi;
-            var color = "";
-            if (uv <= 2.99) {
-                color = "chartreuse";
-            } else if (uv <= 5.99) {
-                color = "yellow";
-            } else if (uv <= 7.99) {
-                color = "orange";
-            } else if (uv <= 10.99) {
-                color = "red";
-            } else {
-                color = "purple";
+            let todaysDate = moment().format('dddd, MMMM Do, YYYY');
+            let today= $("<h1>").text(searchValue[0].toUpperCase() + searchValue.slice(1) + " " + todaysDate);
+            let temperature = $("<p>").text(Math.round(uvData.current.temp) + "˚F");
+            let humidity = $("<p>").text("Humidity: " + uvData.current.humidity + "%");
+            let windSpeed = $("<p>").text(" Wind Speed: " + Math.round(uvData.current.wind_speed) + " mph");
+            let uvI= $("<p>").text("UV Index: " + uvData.current.uvi);
+            $("#today").append(today, temperature, humidity, windSpeed, uvI)
+            for (let i = 0; i < 8; i++) {
+                var uv = uvData.daily[i].uvi;
+                var color = "";
+                if (uv <= 2.99) {
+                    color = "chartreuse";
+                } else if (uv <= 5.99) {
+                    color = "yellow";
+                } else if (uv <= 7.99) {
+                    color = "orange";
+                } else if (uv <= 10.99) {
+                    color = "red";
+                } else {
+                    color = "purple";
+                }
+                $(`#day-${i}`).append("<p id='uv-paragraph'>UV Index:</p><div class='uvI' id='uv-index' style='color: " + color + "'>" + uv + "</div>")
             }
-            $(`#day-${i}`).append("<p id='uv-paragraph'>UV Index:</p><div class='uvI' id='uv-index' style='color: " + color + "'>" + uv + "</div>")
-        }
-    });
-        
+        });
 }
-
+function searchedCities() {
+      let ls = ($("#citiesSearched").append($("<p class='city-searched'>").text("Weather changes! Go ahead and double check it!")));
+      ls.append($("<li>").val(searchValue));
+}
+searchedCities();
 
 // event listeners
 $("#search-button").on("click", function (event) {
@@ -70,4 +81,6 @@ $("#search-button").on("click", function (event) {
     event.preventDefault();
     searchValue = $("#search-value").val().trim();
     weather();
+    localStorage.setItem('city', (searchValue));
+    $("#search-value").val("");
 })
